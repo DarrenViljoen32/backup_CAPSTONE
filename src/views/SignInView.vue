b1d420be6555ffa65728bc4de0e73cf01531d9ec
Darren Viljoen <darrenviljoen32@gmail.com>
2024-03-19 15:22:42 +0200

    new commit

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="view">
         <h1>Sign Up</h1>
-        <input type="text" name="admin_Name" id="admin_Name2" placeholder="Name" v-model="admin_Name">
+        <input type="text" name="user_Name" id="user_Name2" placeholder="Name" v-model="user_Name">
         <br><br>
-        <input type="text" name="admin_Email" id="admin_Email" placeholder="Email" v-model="admin_Email">
+        <input type="text" name="user_Surname" id="user_Surname2" placeholder="Surname" v-model="user_Surname">
         <br><br>
-        <input type="password" name="admin_Password" id="admin_Password2" placeholder="Password" v-model="admin_Password">'
+        <input type="text" name="user_Age" id="user_Age" placeholder="Age" v-model="user_Age">
+        <br><br>
+        <input type="text" name="user_Gender" id="user_Gender" placeholder="Gender" v-model="user_Gender">
+        <br><br>
+        <input type="text" name="user_Email" id="user_Email" placeholder="Email" v-model="user_Email">
+        <br><br>
+        <input type="password" name="user_Password" id="user_Password2" placeholder="Password" v-model="user_Password">
+        <br><br>
+        <input type="text" name="user_Role" id="user_Role" placeholder="Role" v-model="user_Role">
+        <br><br>
+        <input type="text" name="user_Image" id="user_Image" placeholder="Image Url" v-model="user_Image">
         <br><br>
         
         <router-link to="/login"><button @click="plusAdmin()">Register</button></router-link>
@@ -21,15 +31,21 @@
 export default{
     data() {
         return{
-            admin_Name: null,
+            user_Name: null,
+            user_Surname: null,
+            user_Age: null,
+            user_Gender: null,
             admin_Email: null,
-            admin_Password: null
+            admin_Password: null,
+            admin_Role: null,
+            admin_Image: null
+
         }
     },
 
     methods:{
         plusAdmin(){
-            this.$store.dispatch('addAdmin', this.$data)
+            this.$store.dispatch('addUser', this.$data)
         }
     },
 
